58e428927228da459cefe579f46a1011d38c9036
alexis.rueda <alexisrueda34@gmail.com>
2026-06-18 22:17:54 -0600

    Agregar soporte para autenticación OAuth2 y modificar la gestión de pedidos por usuario en OrderService

--- src/main/java/com/ecommerce/order_service/controller/OrderController.java
+++ src/main/java/com/ecommerce/order_service/controller/OrderController.java
@@ -6,38 +6,54 @@ import com.ecommerce.order_service.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/order") // Versionado v1
+@RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 public class OrderController {
-
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderResponse placeOrder(@Valid @RequestBody OrderRequest orderRequest) {
-        return orderService.placeOrder(orderRequest);
+    public OrderResponse placeOrder(@Valid @RequestBody OrderRequest orderRequest,
+                                    @AuthenticationPrincipal Jwt jwt
+    ){
+        return orderService.placeOrder(orderRequest, jwt.getSubject());
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<OrderResponse> getAllOrders() {
-        return orderService.getAllOrders();
+    public List<OrderResponse> getOrders(@AuthenticationPrincipal Jwt jwt){
+        String userId = jwt.getSubject();
+
+        boolean isAdmin = false;
+
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+
+        if(realmAccess!=null && realmAccess.containsKey("roles")){
+            List<String> roles = (List<String>) realmAccess.get("roles");
+            isAdmin = roles.stream().anyMatch(role -> role.equalsIgnoreCase("ADMIN") );
+        }
+
+        return orderService.getOrders(userId, isAdmin);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public OrderResponse getOrderById(@PathVariable Long id) {
+    public OrderResponse getOrderById(@PathVariable Long id){
         return orderService.getOrderById(id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrder(@PathVariable Long id) {
+    public void deleteOrder(@PathVariable Long id){
         orderService.deleteOrder(id);
     }
+
 }
